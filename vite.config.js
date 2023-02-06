e1a33649f210eea4b62164ddb288e82945c78011
hamzanajeeb-tp <hamza.najeeb@10pearls.com>
2023-02-06 16:49:28 +0500

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vite-react-project/',
+  base: '/vite-react-project/src',
   plugins: [react()],
 })
